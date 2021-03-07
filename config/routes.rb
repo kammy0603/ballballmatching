02f5d0214cb48c0e672857a1072def19d0ef3ab8
kammy0603 <wingzero0127@gmail.com>
2021-03-07 10:17:37 +0900

    TOP制作

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,6 @@ Rails.application.routes.draw do
   root to: "teams#top"
   resources :users do
     resources :teams
-  devise_for :users, :controllers => {
-    :registrations => 'users/registrations',
-    :sessions => 'users/sessions'
-     }
+
   end
 end
